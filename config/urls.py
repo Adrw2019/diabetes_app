146387ec5fb7ca59a966e95054b778e5efd0163b
unknown <tu_correo@dominio.com>
2026-03-14 22:38:09 -0500

    fix urls after login

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from diabetes_app import views
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.predict_form, name='home'),
+    path("admin/", admin.site.urls),
+    path("", include("diabetes_app.urls")),
 ]
